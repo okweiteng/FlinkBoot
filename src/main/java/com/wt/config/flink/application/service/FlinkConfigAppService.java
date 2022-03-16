5fb7241348495bb09f93ac62afb897d6ff3becc6
okweiteng <okweiteng@gmail.com>
2022-03-16 23:33:07 +0800

    add retry

--- src/main/java/com/wt/config/flink/application/service/FlinkConfigAppService.java
+++ src/main/java/com/wt/config/flink/application/service/FlinkConfigAppService.java
@@ -20,7 +20,10 @@ package com.wt.config.flink.application.service;
 
 import com.wt.config.flink.application.dto.FlinkConfigDto;
 import com.wt.config.flink.domain.service.FlinkConfigRepository;
+import com.wt.config.retry.RetryableProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,7 +35,14 @@ import java.util.stream.Collectors;
 public class FlinkConfigAppService {
     private final FlinkConfigRepository repository;
 
+    private final RetryableProperties retryableProperties;
+
     @Transactional(readOnly = true)
+    @Retryable(maxAttemptsExpression = "#{@retryableProperties.maxAttempts}",
+            backoff = @Backoff(
+                    delayExpression = "#{@retryableProperties.delay}",
+                    maxDelayExpression = "#{@retryableProperties.maxDelay}",
+                    multiplierExpression = "#{@retryableProperties.multiplier}"))
     public List<FlinkConfigDto> findAll() {
         return repository.findAll().stream()
                 .map(FlinkConfigDto::new)
